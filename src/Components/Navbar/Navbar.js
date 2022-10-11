9263d9201cfad392408c168a8dc7b50ab9da9406
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-10-12 03:08:23 +0600

    navbar fixing

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className="bg-slate-800 text-white">
+    <nav className="bg-slate-800 text-white pt-2 pb-2">
       <div className="md:w-4/5 m-auto">
         <div>
           <div
@@ -18,24 +18,24 @@ const Navbar = () => {
 
         <div className="flex justify-between items-center py-2 bg-slate-800 text-white">
           <div>
-            <h1 className="text-3xl">Quick Quiz</h1>
+            <h1 className="text-4xl">Quick Quiz</h1>
           </div>
 
           <div
-            className={`bg-slate-800 w-1/2 md:text-lg text-white w-full md:flex justify-center absolute md:static duration-500 ease-in ${
-              menuOpen ? "top-10" : "top-[-120px]"
+            className={`bg-slate-800 w-1/2   md:text-lg text-white  md:flex justify-center absolute md:static duration-500 ease-in ${
+              menuOpen ? "top-10 pb-3" : "top-[-120px]"
             }`}
           >
-            <Link className="pl-5" to={"/home"}>
+            <Link className="pl-5 text-xl" to={"/home"}>
               Home
             </Link>
-            <Link className="pl-5" to={"/statistics"}>
+            <Link className="pl-5 text-xl" to={"/statistics"}>
               Statistics
             </Link>
-            <Link className="pl-5" to={"/blog"}>
+            <Link className="pl-5 text-xl" to={"/blog"}>
               Blog
             </Link>
-            <Link className="pl-5" to={""}>
+            <Link className="pl-5 text-xl" to={""}>
               About
             </Link>
           </div>
